9243f5036921aaca123146a98f689057e331c575
Fjodor Ekström <fjodor.ekstrom@jayway.com>
2013-05-29 18:55:31 +0200

    jquery like getJSON()

--- todos/scripts/todo.js
+++ todos/scripts/todo.js
@@ -1,3 +1,10 @@
-Zepto(function($){
-	alert('Page ready');
+$(document).ready(function(){
+	$("button").click(function(){
+		$.getJSON("http://localhost:9090/todo_items.json",function(result){
+			$.each(result, function(i, field){
+				$("div").html("<p>" + field + "</p>");
+			});
+		});
+	});
 });
+
--- todos/scripts/todoModel.js
+++ todos/scripts/todoModel.js
@@ -1,39 +1,13 @@
-$(document).ready(function(){
-	$('.link').on('click', getJSON("http://localhost:9090/todo_items/1.json"));
+(function($){
+	var Todo = Backbone.Model.extend({
+		el: $('.todo'),
+		initialize: {
+			_.bindAll(this, 'render');
+		},
+		render: {
+			$(this.el).append("<ul><li>hello world</li></ul>");
+		}
+	});
 
-	function getJSON(url) {
-		var res;
-		$.ajax({
-			type: 'GET',
-			crossDomain: true,
-			url: url,
-			dataType: 'json',
-			cache: false,
-			success: function(data) {
-				alert(data);
-			},
-			error: function(xhr, type, error) {
-				//error code here
-			},
-			complete: function() {
-				// on complete code here
-				console.log("Successfully fetched " + res);
-			}
-		});
-	}
-
-		function getJSON2(urlen){
-		$.getJSON(urlen, function(data) {
-			var items = [];
-
-			$.each(data, function(key, val) {
-				items.push('<li id="' + key + '">' + val + '</li>');
-			});
-
-			$('<ul/>', {
-				'class': 'my-new-list',
-				html: items.join('')
-			}).appendTo('body');
-		});
-	}
-});
+	var todo = new Todo();
+})(Zepto);
