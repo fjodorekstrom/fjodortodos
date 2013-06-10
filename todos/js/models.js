71b2ef0187a8724045fe9dbee66576502436c2f8
Fjodor Ekström <fjodor.ekstrom@jayway.com>
2013-06-10 18:40:12 +0200

    now logging all models when fetch is done

--- todos/js/models.js
+++ todos/js/models.js
@@ -1,27 +1,34 @@
-$(function() {
-	var Todo = Backbone.Model.extend({
-		defaults: {
-			name: "name",
-			description: "description",
-			completed: false
-		},
-		initialize: function(){
-			this.on('change', function(){
-				console.log("model changed");
-			});
-		}
-	});
-
-	var TodosCollection = Backbone.Collection.extend({
-		model: Todo,
-		name: "todo_items",
-		url: 'http://localhost/api/todo_items'
-	});
-
-	var Todos = new TodosCollection();
-	Todos.fetch().done();
-
-	console.log(Todos);
-	var models = Todos['models'];
-	console.log(models.length);
+var Todo = Backbone.Model.extend({
+	defaults: {
+		name: "name",
+		description: "description",
+		completed: false
+	},
+	initialize: function(){
+		this.on('change', function(){
+			console.log("model changed");
+		});
+	}
 });
+
+var TodosCollection = Backbone.Collection.extend({
+	model: Todo,
+	url: 'http://localhost/api/todo_items',
+
+	initialize: function(){
+		console.log("Todos initialized");
+	}
+});
+
+var Todos = new TodosCollection();
+Todos.fetch();
+Todos.fetch().done(function(){
+	Todos.forEach(function(model){
+		console.log(model.attributes.name);
+		console.log(model.attributes.description);
+		console.log(" ");
+	});
+});
+
+console.log(Todos.models.length);
+console.log(Todos);
