7145768c6a07480c18e9e988d3b5abde029f6fa4
Fjodor Ekström <fjodor.ekstrom@jayway.com>
2013-06-18 10:15:42 +0200

    added remove and complete functionality

--- todos/js/views/todoView.js
+++ todos/js/views/todoView.js
@@ -8,17 +8,41 @@ app.TodoView = Backbone.View.extend({
 	events: {
 		'dblclick label': 'edit',
 		'keypress .edit': 'updateOnEnter',
-		'blur .edit': 'close'
+		'blur .edit': 'close',
+		'click .toggle': 'togglecompleted',
+		'click .destroy': 'clear'
 	},
 
 	initialize: function() {
 		this.listenTo(this.model, 'change', this.render);
+		this.listenTo(this.model, 'destroy', this.remove);
+		this.listenTo(this.model, 'visible', this.toggleVisible);
 	},
 
 	render: function() {
 		this.$el.html( this.template( this.model.toJSON() ) );
+
+		this.$el.toggleClass( 'completed', this.model.get('completed') );
+		this.toggleVisible();
+
 		this.$input = this.$('.edit');
 		return this;
+	},
+
+	toggleVisible: function() {
+		this.$el.toggleClass( 'hidden', this.isHidden());
+	},
+
+	isHidden: function() {
+		var isCompleted = this.model.get('completed');
+		return( 
+			(!isCompleted && app.TodoFilter === 'completed')
+			|| (isCompleted && app.TodoFilter === 'active')
+			);
+	},
+
+	togglecompleted: function() {
+		this.model.toggle();
 	},
 
 	edit: function() {
@@ -30,6 +54,8 @@ app.TodoView = Backbone.View.extend({
 		var value = this.$input.val().trim();
 		if( value ) {
 			this.model.save({ title: value });
+		} else {
+			this.clear();
 		}
 
 		this.$el.removeClass('editing');
@@ -39,5 +65,9 @@ app.TodoView = Backbone.View.extend({
 		if ( e.which === ENTER_KEY ) {
 			this.close();
 		}
+	},
+
+	clear: function() {
+		this.model.destroy();
 	}
 });
